84731e9daad022db1601e79b634f8046abb4e676
Asheeqa Ali123 <shasha7880@gmail.com>
2024-04-26 21:51:51 +0500

    first commit

---
+++ atm.js
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+let myBalance = 20000;
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt({
+    name: "pin",
+    message: "Enter ur pin code",
+    type: "number",
+});
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+    let actionAns = await inquirer.prompt({
+        name: "action",
+        message: "Please select an option",
+        type: "list",
+        choices: ["withdraw", "check balance"],
+    });
+    if (actionAns.action === "withdraw") {
+        let amountAns = await inquirer.prompt({
+            name: "amount",
+            message: "Enter the amount u wanna withdraw",
+            type: "number",
+        });
+        if (myBalance >= amountAns.amount) {
+            myBalance -= amountAns.amount;
+            console.log(`Your remaining balance is ${myBalance}`);
+        }
+        else {
+            console.log("Insufficiant balance");
+        }
+    }
+    let afterWithdraw = await inquirer.prompt({
+        name: "Question",
+        message: "Do u wanna check your balance or not?",
+        choices: ["Yup", "Nope"],
+        type: "list",
+    });
+    if (afterWithdraw.Question == "Yup") {
+        console.log(`Your total balance is ${myBalance} `);
+    }
+}
+else {
+    console.log("opps! Incorrect pin code");
+}
